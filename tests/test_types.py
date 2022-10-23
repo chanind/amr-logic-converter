94b733f5b8dfa9e5098049cdfe08461bed697421
David Chanin <chanindav@gmail.com>
2022-10-23 18:11:41 +0100

    feat: removing Const and Param types from Formula

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,4 +1,4 @@
-from amr_logic_converter.types import Const, And, Implies, Or
+from amr_logic_converter.types import Const, And, Implies, Or, Predicate
 
 
 def test_const_strips_quotes_from_strings() -> None:
@@ -8,30 +8,88 @@ def test_const_strips_quotes_from_strings() -> None:
 
 
 def test_and_spreads_nested_ands() -> None:
-    and_ = And((And((Const("a", "symbol"), Const("b", "symbol"))),))
-    assert and_.args == (Const("a", "symbol"), Const("b", "symbol"))
-    assert str(and_) == "a ∧ b"
+    and_ = And(
+        (
+            And(
+                (
+                    Predicate("P", (Const("a", "symbol"),)),
+                    Predicate("P", (Const("b", "symbol"),)),
+                )
+            ),
+        ),
+    )
+    assert and_.args == (
+        Predicate("P", (Const("a", "symbol"),)),
+        Predicate("P", (Const("b", "symbol"),)),
+    )
+    assert str(and_) == "P(a) ∧ P(b)"
 
 
 def test_or_spreads_nested_ors() -> None:
-    or_ = Or((Or((Const("a", "symbol"), Const("b", "symbol"))),))
-    assert or_.args == (Const("a", "symbol"), Const("b", "symbol"))
-    assert str(or_) == "a ∨ b"
+    or_ = Or(
+        (
+            Or(
+                (
+                    Predicate("P", (Const("a", "symbol"),)),
+                    Predicate("P", (Const("b", "symbol"),)),
+                )
+            ),
+        )
+    )
+    assert or_.args == (
+        Predicate("P", (Const("a", "symbol"),)),
+        Predicate("P", (Const("b", "symbol"),)),
+    )
+    assert str(or_) == "P(a) ∨ P(b)"
 
 
 def test_or_adds_parens_to_nested_ands_when_printing() -> None:
-    or_ = Or((And((Const("a", "symbol"), Const("b", "symbol"))), Const("c", "symbol")))
-    assert str(or_) == "(a ∧ b) ∨ c"
+    or_ = Or(
+        (
+            And(
+                (
+                    Predicate("P", (Const("a", "symbol"),)),
+                    Predicate("P", (Const("b", "symbol"),)),
+                )
+            ),
+            Predicate(
+                "P",
+                (Const("c", "symbol"),),
+            ),
+        )
+    )
+
+    assert str(or_) == "(P(a) ∧ P(b)) ∨ P(c)"
 
 
 def test_and_adds_parens_to_nested_ors_when_printing() -> None:
-    and_ = And((Or((Const("a", "symbol"), Const("b", "symbol"))), Const("c", "symbol")))
-    assert str(and_) == "(a ∨ b) ∧ c"
+    and_ = And(
+        (
+            Or(
+                (
+                    Predicate("P", (Const("a", "symbol"),)),
+                    Predicate("P", (Const("b", "symbol"),)),
+                )
+            ),
+            Predicate("P", (Const("c", "symbol"),)),
+        )
+    )
+    assert str(and_) == "(P(a) ∨ P(b)) ∧ P(c)"
 
 
 def test_imples_adds_parens_to_nested_clauses_when_printing() -> None:
     implies = Implies(
-        And((Or((Const("a", "symbol"), Const("b", "symbol"))), Const("c", "symbol"))),
-        Const("d", "symbol"),
+        And(
+            (
+                Or(
+                    (
+                        Predicate("P", (Const("a", "symbol"),)),
+                        Predicate("P", (Const("b", "symbol"),)),
+                    )
+                ),
+                Predicate("P", (Const("c", "symbol"),)),
+            )
+        ),
+        Predicate("P", (Const("d", "symbol"),)),
     )
-    assert str(implies) == "((a ∨ b) ∧ c) → d"
+    assert str(implies) == "((P(a) ∨ P(b)) ∧ P(c)) → P(d)"
